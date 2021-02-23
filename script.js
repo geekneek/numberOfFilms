73d1ea669404d2e2c0d2efbfb234bbec241d086b
Nike <geekneek28@gmail.com>
2021-02-23 14:34:42 +0200

    numberOfFilms with loop

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strick';
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
@@ -8,13 +10,28 @@ const personalMovieDB = {
    privar: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = +prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = +prompt('На сколько оцените его?', '');
+if (personalMovieDB.count < 10) {
+   alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count < 30) {
+   alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+   alert('Вы киноман');
+} else {
+   alert('Произошла ошибка');
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+   const a = prompt('Один из последних просмотренных фильмов?', ''),
+         b = +prompt('На сколько оцените его?', '');
+   if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log('DONE!');
+   } else {
+      console.log('ERROR!');
+      i--;
+   }
+}
 
 console.log(personalMovieDB);
 
